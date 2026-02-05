e3a3462d13f53a2288160f4858d9c501d1648570
ge94caf <ge94caf@tum.de>
2026-02-05 18:45:24 +0100

    Finished solver system matrix validation and function to check if system matrix for Cg is symmetric. Co-authored-by: Yuhsiang M. Tsai <yhmtsai@gmail.com>

--- core/base/validation.hpp
+++ core/base/validation.hpp
@@ -13,6 +13,11 @@
 #include <ginkgo/core/base/exception.hpp>
 #include <ginkgo/core/base/math.hpp>
 #include <ginkgo/core/base/temporary_clone.hpp>
+#include <ginkgo/core/matrix/coo.hpp>
+#include <ginkgo/core/matrix/csr.hpp>
+#include <ginkgo/core/matrix/diagonal.hpp>
+#include <ginkgo/core/matrix/ell.hpp>
+#include <ginkgo/core/matrix/permutation.hpp>
 
 
 namespace gko {
@@ -136,7 +141,7 @@ void validate_system_matrix(std::shared_ptr<const LinOp> mtx)
     }
 
     auto try_validate = [&](auto&& ptr, const char* name) {
-        using PtrType = decltype(ptr);
+        using PtrType = typename std::remove_reference<decltype(ptr)>::type;
         if (auto typed =
                 std::dynamic_pointer_cast<const typename PtrType::element_type>(
                     mtx)) {
